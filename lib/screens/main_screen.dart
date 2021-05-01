d7ba4714934aa85e46a47f98355341d96626df28
Vitor Azevedo <vitor.azevedo009@gmail.com>
2021-05-01 17:03:21 -0300

    add api logic

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MainPage extends StatelessWidget {
+  static var url = Uri.parse("https://blockchain.info/ticker");
+
+  void getCurrencies() async {
+    var res = await http.get(url);
+    Map<String, dynamic> data = json.decode(res.body);
+    print(data['BRL']['buy']);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +24,9 @@ class MainPage extends StatelessWidget {
             children: [
               Text('Crypto'),
               Text('Convencional'),
-              ElevatedButton(
-                onPressed: () {},
-                child: Icon(Icons.add),
+              TextButton(
+                onPressed: getCurrencies,
+                child: Text('R\$'),
               ),
             ],
           ),
