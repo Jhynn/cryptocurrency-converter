557cd6e3180904b77bb06719fd168e07e3653f62
Matheus Miranda <rock.matheus.mh@gmail.com>
2021-05-03 17:17:07 -0300

    So falta o rodape.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -4,94 +4,140 @@ import 'package:http/http.dart' as http;
 
 import 'dart:convert';
 
-class MainPage extends StatefulWidget {
-  static var url = Uri.parse("https://blockchain.info/ticker");
+import '../model/data.dart';
+// 9e8d236a372d8e3e64a86239d5d6d5509c0e82bbf086c6f72504714c28803846 API key
 
+class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
-  String symbol = '';
-  String country = 'USD';
+  String currency = 'Selecione uma moeda';
   double price = 0.0;
-  Map<String, dynamic> d = Map<String, dynamic>();
+  Data data = Data();
+
+  Map<String, dynamic> d = {
+    "BTC": {"BRL": 0},
+    "ETH": {"BRL": 0},
+    "XRP": {"BRL": 0},
+  };
 
   void getCurrencies() async {
-    http.Response response = await http.get(MainPage.url);
+    var url = Uri.parse(
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP&tsyms=$currency");
+    http.Response response = await http.get(url);
     Map<String, dynamic> data = json.decode(response.body);
     setState(() {
       d = data;
-      symbol = data[country]['symbol'];
-      price = data[country]['buy'];
     });
+  }
+
+  void bottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return Container(
+          color: ThemeData.dark().primaryColor,
+          child: ListView.builder(
+            itemCount: data.currencies.length,
+            itemBuilder: (context, index) {
+              // var k = d.keys.toList();
+              return ListTile(
+                leading: Icon(Icons.emoji_flags),
+                title: Text(data.currencies[index]),
+                onTap: () {
+                  currency = data.currencies[index];
+                  getCurrencies();
+                },
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    getCurrencies();
     return Scaffold(
       appBar: AppBar(
         title: Text('Cyptocurrency Converter'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            children: [
-              SizedBox(height: 17.0),
-              Text('Crypto'),
-            ],
-          ),
-          Column(
-            children: [
-              SizedBox(height: 17.0),
-              Text('Convencional - $symbol'),
-              Container(
-                child: ListView.builder(
-                  itemCount: d.length,
-                  itemBuilder: (context, index) {
-                    var k = d.keys.toList();
-                    return ListTile(
-                      leading: Icon(Icons.emoji_flags),
-                      title: Text(d["BRL"]["buy"]),
-                      onTap: () {
-                        country = k[index];
-                      },
-                    );
-                  },
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('Bitcoin (BTC)'),
+                    TextButton(
+                      child: Text('$currency - ${d["BTC"][currency]}'),
+                      onPressed: bottomSheet,
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('Ethereum (ETH)'),
+                    TextButton(
+                      child: Text('$currency - ${d["ETH"][currency]}'),
+                      onPressed: bottomSheet,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('Monero (XRP)'),
+                    TextButton(
+                      child: Text('$currency - ${d["XRP"][currency]}'),
+                      onPressed: bottomSheet,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Column(
+              children: [
+                Text('Matheus Henrique Silva Miranda'),
+                Text('Phillippy Cardelly Albuquerque dos Santos'),
+                Text('Vitor Azevedo Silva'),
+                Text('Wesley Costa da Silva'),
+              ],
+            ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: ThemeData.dark().primaryColor,
         foregroundColor: ThemeData.dark().accentColor,
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext bc) {
-              return Container(
-                color: ThemeData.dark().primaryColor,
-                child: ListView.builder(
-                  itemCount: d.length,
-                  itemBuilder: (context, index) {
-                    var k = d.keys.toList();
-                    return ListTile(
-                      leading: Icon(Icons.emoji_flags),
-                      title: Text(k[index]),
-                      onTap: () {
-                        country = k[index];
-                      },
-                    );
-                  },
-                ),
-              );
-            },
-          );
-        },
+        onPressed: bottomSheet,
         child: Icon(
           Icons.arrow_upward,
         ),
